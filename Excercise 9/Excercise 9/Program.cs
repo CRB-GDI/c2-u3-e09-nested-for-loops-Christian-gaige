824a0c71663fe89add9cf421b31573a07d622614
Christian-gaige <145484661+Christian-gaige@users.noreply.github.com>
2024-03-06 13:05:57 -0500

    Exercise 9

---
+++ Excercise 9/Excercise 9/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Transactions;
+
+namespace Excercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a row width: ");
+            int width = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many number of rows?");
+            int rows = Int32.Parse(Console.ReadLine());
+
+            //print rows going down
+            Console.WriteLine();
+            for (int i = 1; i <= rows; i++)
+            {
+                //inner for loop printing the width
+                for (int j = 1; j <= width; j++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("What is the Max  number of asterisks?");
+            int MaxAster = Int32.Parse(Console.ReadLine());
+
+            //print rows going down
+            Console.WriteLine();
+            for (int i = 1; i <= MaxAster; i++)
+            {
+                //inner for loop printing the width
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
